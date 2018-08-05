66528e3cb1cd5c0d8976071d052301f5c872e641
Unknown <cunha.passos@gmail.com>
2018-08-05 03:40:23 -0300

    vamos que vamos

--- index.js
+++ index.js
@@ -165,7 +165,8 @@ app.use(gestorRouter);
 var server = http.createServer(app);
 app.io.attach(server);
 
-server.listen('3000', () => {
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
 	console.log("server started");
 })
 
