649719c910dc7d1e29c8598416ff0db0da51e39b
Paulo Passos <cunha.passos@gmail.com>
2018-08-04 23:04:22 -0300

    agora vai

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var knex = require('knex')({
   client: 'pg',
   version: '10.3',
   connection: {
-    host : 'localhost',
+    host : 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq',
     user : 'postgres',
     password : 'postgres',
     database : 'ProjetoMDS'
