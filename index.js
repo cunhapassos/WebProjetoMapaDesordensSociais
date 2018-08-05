0e0218a33d3d13692e9e3908b272375f3a82d953
Unknown <cunha.passos@gmail.com>
2018-08-05 05:39:49 -0300

    mais uma vez

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ var knex = require('knex')({
   client: 'pg',
   version: '10.3',
   connection: {
-    host : '127.0.0.1' || 'localhost',
-    user : 'paulopassos',
-    password : '',
+    //host : '127.0.0.1' || 'localhost',
+    //user : 'paulopassos',
+    //password : '',
     database : process.env.DATABASE_URL || 'ProjetoMDS'
   }
 });
