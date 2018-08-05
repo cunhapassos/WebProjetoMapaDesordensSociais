7d9bf6e9570e68f75451c3557cf9b77a80f652eb
Unknown <cunha.passos@gmail.com>
2018-08-05 03:25:36 -0300

    vamos la

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ var knex = require('knex')({
   client: 'pg',
   version: '10.3',
   connection: {
-    host : 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq',
+    host : 'localhost',
     user : 'postgres',
     password : 'postgres',
-    database : 'ProjetoMDS'
+    database : 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
   }
 });
 
