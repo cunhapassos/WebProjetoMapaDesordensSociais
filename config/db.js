a0816acdf1f9be5c5e50945dae839c64a91c1167
Unknown <cunha.passos@gmail.com>
2018-08-18 09:23:33 -0300

    Comentando acesso ao BD local e desconectando acesso ao BD do Heroku

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//   database:{
-//   	client: 'pg',
-//     connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
+module.exports = {
+   database:{
+   	client: 'pg',
+     connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
 
-//   }
-// }
-
+   }
+ }
+/*
 module.exports = {
   database:{
   	client: 'pg',
@@ -17,5 +17,5 @@ module.exports = {
       database : 'ProjetoMDS'
     }
   }
-}
+}*/
 
