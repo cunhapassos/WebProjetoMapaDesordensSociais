d588a60e29343d2f82ed751d6a96171a11131ac0
bruno <brunocordeiro180@gmail.com>
2018-08-17 22:07:52 -0300

    Graficos de pizza adicionados

--- index.js
+++ index.js
@@ -53,10 +53,21 @@ app.use(session({
 }));
 
 //CONEXÃO COM O BANCO
-var knex = require('knex')({
-  client: 'pg',
-  connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
+// var knex = require('knex')({
+//   client: 'pg',
+//   connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
 
+// });
+
+var knex = require('knex')({
+	client: 'pg',
+	version: '10.3',
+	connection: {
+		host : 'localhost',
+		user : 'postgres',
+		password : 'postgres',
+		database : 'ProjetoMDS'
+	}
 });
 
 const st = knexPostgis(knex);
@@ -99,7 +110,7 @@ app.post("/login", function(req,res){
 	var email = req.body.email;
 	
 	console.log(email);
-	// senha = md5(senha);
+	senha = md5(senha);
 
 	var name = 0;
 
