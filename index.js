ccc55cb9387f19e3b295273a7a3510d7225b0377
bruno <brunocordeiro180@gmail.com>
2018-09-10 19:35:13 -0300

    knex apontando para banco da amazon

--- index.js
+++ index.js
@@ -26,22 +26,22 @@ pgconf.defaults.ssl = true
 
 
 // CONEXÃO COM O BANCO
-//  var knex = require('knex')({
-//    client: 'pg',
-//    connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
+ var knex = require('knex')({
+   client: 'pg',
+   connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
 
-//  });
+ });
 
-var knex = require('knex')({
-	client: 'pg',
-	version: '10.3',
-	connection: {
-		host : 'localhost',
-		user : 'postgres',
-		password : 'postgres',
-		database : 'ProjetoMDS'
-	}
-});
+// var knex = require('knex')({
+// 	client: 'pg',
+// 	version: '10.3',
+// 	connection: {
+// 		host : 'localhost',
+// 		user : 'postgres',
+// 		password : 'postgres',
+// 		database : 'ProjetoMDS'
+// 	}
+// });
 
 const st = knexPostgis(knex);
 
