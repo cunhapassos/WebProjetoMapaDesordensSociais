ea5ddcc3e2df46cfc72e6a23148c77fbb492d452
bruno <brunocordeiro180@gmail.com>
2018-09-12 14:35:37 -0300

    reajustadas configuracoes do banco

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ const pgconf = require('pg')
 pgconf.defaults.ssl = true
 
 
-// CONEXÃO COM O BANCO
-//  var knex = require('knex')({
-//    client: 'pg',
-//    connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
+CONEXÃO COM O BANCO
+ var knex = require('knex')({
+   client: 'pg',
+   connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
 
-//  });
+ });
 
-var knex = require('knex')({
-	client: 'pg',
-	version: '10.3',
-	connection: {
-		host : 'localhost',
-		user : 'postgres',
-		password : 'postgres',
-		database : 'ProjetoMDS'
-	}
-});
+// var knex = require('knex')({
+// 	client: 'pg',
+// 	version: '10.3',
+// 	connection: {
+// 		host : 'localhost',
+// 		user : 'postgres',
+// 		password : 'postgres',
+// 		database : 'ProjetoMDS'
+// 	}
+// });
 
 const st = knexPostgis(knex);
 
