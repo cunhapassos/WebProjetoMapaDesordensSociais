8921200c76c19c8a1e3641d26d88fefe48b11187
bruno <brunocordeiro180@gmail.com>
2018-09-12 15:05:51 -0300

    erro corrigido

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const pgconf = require('pg')
 pgconf.defaults.ssl = true
 
 
-CONEXÃO COM O BANCO
+// CONEXÃO COM O BANCO
  var knex = require('knex')({
    client: 'pg',
    connection: 'postgres://jwxsimyoqvdxew:5b045716d82e36a180af30cd8bbfd5ad6d2a3fecc2a6ae8db7f3e49f9242222c@ec2-54-221-210-97.compute-1.amazonaws.com:5432/d40ba6n3knjjq'
